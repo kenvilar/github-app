41d7486a3cf5c0361705285b8117d0f1ace2f509
Ken Vilar <kenvilar@gmail.com>
2018-04-23 00:56:37 +0800

    Setup github api with axios

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,6 +2,16 @@ import Vue from 'vue';
 
 Vue.config.debug = process.env.NODE_ENV !== 'production';
 
+import Axios from 'axios';
+
+Axios.defaults.headers.common.Accept = 'application/vnd.github.v3+json';
+
+Object.defineProperty(Vue.prototype, '$http', {
+  get: function() {
+    return Axios;
+  }
+});
+
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
